f7e41ae77ea856db73eb9c801ef909ea8810e089
Daniel Crampton <dcrampton@gmail.com>
2015-11-21 01:00:09 -0800

    Remove useless code

--- VerticalCards/ViewController.m
+++ VerticalCards/ViewController.m
@@ -12,7 +12,6 @@
 @interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
-@property (nonatomic, strong) NSArray *colours;
 
 @end
 
@@ -26,17 +25,6 @@
     DCCardLayout *layout = [[DCCardLayout alloc] init];
     layout.minimumLineSpacing = 0.0f;
     self.collectionView.collectionViewLayout = layout;
-    
-    NSMutableArray *temp = [@[] mutableCopy];
-    for (int x = 0; x < 100000; x++) {
-        CGFloat hue = ( arc4random() % 256 / 256.0 );
-        CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;
-        CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;
-        UIColor *colour = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
-        [temp addObject:colour];
-    }
-    
-    self.colours = [temp copy];
 }
 
 - (void)viewDidLayoutSubviews
@@ -48,19 +36,16 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.colours.count;
+    return 10;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([UICollectionViewCell class]) forIndexPath:indexPath];
-    UIColor *colour = self.colours[indexPath.item];
     
     UIImageView *catImageView = [[UIImageView alloc] initWithFrame:cell.bounds];
     catImageView.image = [UIImage imageNamed:@"cat"];
     [cell.contentView addSubview:catImageView];
-    
-    cell.contentView.backgroundColor = colour;
     
     return cell;
 }
